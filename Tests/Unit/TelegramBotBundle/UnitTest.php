00a3f29b54eb244a00d8f5d610d89123c84cdc0e
myrkox <mberlad@grossum.com>
2016-09-15 15:29:28 +0300

    TBB-5: updated travis config, added composer.json, added unit test to check config

---
+++ Tests/Unit/TelegramBotBundle/UnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Unit\TelegramBotBundle;
+
+class UnitTest extends \PHPUnit_Framework_TestCase
+{
+	public function testPhpUnitWorks()
+	{
+		$this->assertTrue(true);
+	}
+}
